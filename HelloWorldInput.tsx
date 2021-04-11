de7ce7e33f659940d1918f9adb56934f7a2199fe
mika1407 <koneautomaatio@hotmail.com>
2021-04-12 00:39:27 +0300

    ScrollView lisätty

--- HelloWorldInput.tsx
+++ HelloWorldInput.tsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, ScrollView } from 'react-native';
 
 export default function HelloWorldInput() {
   //HOOKS-muuttujat:
@@ -20,29 +20,31 @@ export default function HelloWorldInput() {
   )
 
   return (
-    <View style={styles.container2}>
-      <View > 
-        <Text style={styles.titleText} >Laskuri:</Text>
+    <ScrollView style={styles.scrollView} fadingEdgeLength={180}>
+      <View style={styles.container2}>
+        <View > 
+          <Text style={styles.titleText} >Laskuri:</Text>
+        </View>
+        <View >
+          <Text style={styles.bigCentered}>{counter}</Text>
+        </View>
+        <View>
+          <Text style={{fontSize: 22 }}>Anna nimi:</Text>
+          <TextInput
+              style = {{height: 40, borderColor: 'gray', backgroundColor: 'white', padding: 4, borderWidth: 1, margin: 3}}
+              onChangeText={text => setName(text)}
+              value={name}
+              placeholder="Nimi tähän"
+          />
+          <Button 
+              title="Lisää henkilö"
+              onPress={() => showName(name)}
+          />
+          {/* <Text style={styles.titleText}>{outputName}</Text> */}
+          <Text style={styles.titleText}>{array}</Text>
+        </View>
       </View>
-      <View >
-        <Text style={styles.bigCentered}>{counter}</Text>
-      </View>
-      <View>
-        <Text style={{fontSize: 22 }}>Anna nimi:</Text>
-        <TextInput
-            style = {{height: 40, borderColor: 'gray', backgroundColor: 'white', padding: 4, borderWidth: 1, margin: 3}}
-            onChangeText={text => setName(text)}
-            value={name}
-            placeholder="Nimi tähän"
-        />
-        <Button 
-            title="Lisää henkilö"
-            onPress={() => showName(name)}
-        />
-        {/* <Text style={styles.titleText}>{outputName}</Text> */}
-        <Text style={styles.titleText}>{array}</Text>
-      </View>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -62,4 +64,8 @@ const styles = StyleSheet.create({
     fontSize: 48,
     textAlign: 'center',
   },
+  scrollView: {
+    width: '100%',
+    marginVertical: 10,
+  }
 });
